db5f8e45d05ac7c9cb11e385aac4237850644c8d
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2023-09-25 17:17:15 +0200

    show a single article on a detail page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # Defines the root path route ("/")
   root "articles#index"
 
   get "/articles", to: "articles#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/articles/:id", to: "articles#show"
 end
